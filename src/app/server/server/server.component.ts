97e3ff86ba7edaedcb44ec5797f65c9f0fc333dc
Vishesh <visheshsingh16@gmail.com>
2018-03-04 11:36:00 -0600

    Property Binding

--- src/app/server/server/server.component.ts
+++ src/app/server/server/server.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
   serverId = 10;
+  serverStatus = 'offline';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getServerStatus(){
+    return this.serverStatus;
   }
 
 }
